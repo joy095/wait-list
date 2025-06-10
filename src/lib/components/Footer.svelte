e9afc3d106ea5f6717202ec26bae77baf1c9f229
Joy Karmakar <joykarmakar987654321@gmail.com>
2025-06-10 17:04:10 +0530

    update

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,9 +1,11 @@
 <footer class="content-section py-8">
 	<div class="container mx-auto px-6 text-center text-gray-500">
-		<p>&copy; {new Date().getFullYear()} Elite Bookings. All Rights Reserved.</p>
-		<div class="mt-4 flex justify-center gap-6">
+		<a href="https://joykarmakar.vercel.app" target="_blank"
+			>&copy; {new Date().getFullYear()} joykarmakar. All Rights Reserved.</a
+		>
+		<!-- <div class="mt-4 flex justify-center gap-6">
 			<a href="#" class="hover:text-yellow-400">Terms of Service</a>
 			<a href="#" class="hover:text-yellow-400">Privacy Policy</a>
-		</div>
+		</div> -->
 	</div>
 </footer>
